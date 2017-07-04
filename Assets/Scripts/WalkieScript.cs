a62cc5d366ee4909c5cc0d7d632bb4dd3be605cb
Unknown <amtaidan@comcast.net>
2017-07-04 12:04:54 -0500

    Billy is now HC. HC can move up and down. 5 HCs

--- Assets/Scripts/WalkieScript.cs
+++ Assets/Scripts/WalkieScript.cs
@@ -17,5 +17,11 @@ public class WalkieScript : MonoBehaviour {
 		jimmy.Move(transform.forward *Time.deltaTime *vertical *speed);
 		jimmy.Move (transform.right * Time.deltaTime * horizontal * speed);
 		transform.Rotate (0, Input.GetAxis ("Mouse X") * Time.deltaTime * 80f, 0);
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.Translate (0f, 3f*Time.deltaTime, 0f);
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			transform.Translate (0f, -3f*Time.deltaTime, 0f);
+		}
 	}
 }
